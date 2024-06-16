e5da9c0a16a86d97c61161d686357dfa89977ab7
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-16 17:12:00 -0400

    add subtest for blank string default

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Brandon")
-	want := "Hello Brandon"
+    t.Run("say Hello to Brandon", func(t *testing.T) {
+        got := Hello("Brandon")
+        want := "Hello Brandon"
 
-	if got != want {
-		t.Errorf("got %q but wanted %q", got, want)
-	}
+        if got != want {
+            t.Errorf("got %q but wanted %q", got, want)
+        }
+    })
+
+    t.Run("say Hello to world by default", func(t *testing.T) {
+        got := Hello("")
+        want := "Hello world"
+
+        if got != want {
+            t.Errorf("got %q but wanted %q", got, want)
+        }
+    })
 }
