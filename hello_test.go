857834f6508a73a5406721cec23cdb8571a57702
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-16 17:35:08 -0400

    Extract test assertion

--- hello_test.go
+++ hello_test.go
@@ -7,17 +7,20 @@ func TestHello(t *testing.T) {
         got := Hello("Brandon")
         want := "Hello Brandon"
 
-        if got != want {
-            t.Errorf("got %q but wanted %q", got, want)
-        }
+        assertCorrectMessage(t, got, want)
     })
 
     t.Run("say Hello to world by default", func(t *testing.T) {
         got := Hello("")
         want := "Hello world"
 
-        if got != want {
-            t.Errorf("got %q but wanted %q", got, want)
-        }
+        assertCorrectMessage(t, got, want)
     })
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+    t.Helper()
+    if got != want {
+        t.Errorf("got %q but wanted %q", got, want)
+    }
 }
