2d1c33ba513ee14732cfceaec98279ae91eeb7a5
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-16 16:50:23 -0400

    test for incoming iteration to address Hello

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello world"
+	got := Hello("Brandon")
+	want := "Hello Brandon"
 
 	if got != want {
 		t.Errorf("got %q but wanted %q", got, want)
