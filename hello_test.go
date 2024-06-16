e0bbc7d161349dc78eb5e580c3e08963b039dbd1
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-16 16:37:42 -0400

    Deliberately break test

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	got := Hello()
-	want := "Hello world"
+	want := "Goodbye world"
 
 	if got != want {
 		t.Errorf("got %q but wanted %q", got, want)
