9675989de5a01b0cec56622ee28d7bc737792f87
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-24 08:40:04 -0400

    Refactor, define Rectangle as struct

--- ch5-structs-methods-and-interfaces/shapes.go
+++ ch5-structs-methods-and-interfaces/shapes.go
@@ -1,11 +1,16 @@
 package main
 
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Perimeter calculates the permiter of a rectangle given a height and width
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Area calculates the area of a rectangle given the width and height
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
