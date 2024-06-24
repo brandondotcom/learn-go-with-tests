1d67e0b8249acc1efa1fd304a928172a2e69154d
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-24 11:31:25 -0400

    Squareable test for type check working with references to shape

--- ch5-structs-methods-and-interfaces/shapes_test.go
+++ ch5-structs-methods-and-interfaces/shapes_test.go
@@ -18,28 +18,28 @@ func TestArea(t *testing.T) {
 		shape        Shape
 		isSquareable bool
 	}{
-		// This test fails - why? or - s it a meaningful test?
-		//{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, isSquareable: true},
-		{name: "Circle", shape: Circle{Radius: 10}, isSquareable: false},
-		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, isSquareable: false},
+		{name: "Rectangle Value", shape: Rectangle{Width: 12, Height: 6}, isSquareable: false /*why?*/},
+		{name: "Rectangle Reference", shape: &Rectangle{Width: 12, Height: 6}, isSquareable: true},
+		{name: "Circle", shape: &Circle{Radius: 10}, isSquareable: false},
+		{name: "Triangle", shape: &Triangle{Base: 12, Height: 6}, isSquareable: false},
 	}
 
 	for _, tt := range areaTests {
 		// using tt.name from the case to use it as the `t.Run` test name
 		t.Run(tt.name, func(t *testing.T) {
-			switch v := tt.shape.GetShape().(type) {
+			switch v := tt.shape.(type) {
 			default:
-				_, got := v.(Squareable)
-				if got != tt.isSquareable {
-					t.Errorf("%T", v)
-				}
+				t.Logf("type of %s, %T", tt.name, v)
 			}
-			s, got := tt.shape.(Squareable)
-			if got {
-				s.MakeSquare()
+			_, squareableCheck := tt.shape.(Squareable)
+			if squareableCheck != tt.isSquareable {
+				t.Errorf("got %t when checking %s for Squareable, expected %t", squareableCheck, tt.name, tt.isSquareable)
 			}
-			if got != tt.isSquareable {
-				t.Errorf("%t", got)
+
+			_, shapeCheck := tt.shape.(Shape)
+
+			if !shapeCheck {
+				t.Errorf("expected %s to implement Shape, but didn't", tt.name)
 			}
 		})
 	}
