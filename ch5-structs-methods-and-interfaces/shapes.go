4eda846024285c3ef8d99633aa0aa510fea57db2
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-24 08:47:23 -0400

    Define Shape interface and Circle struct

--- ch5-structs-methods-and-interfaces/shapes.go
+++ ch5-structs-methods-and-interfaces/shapes.go
@@ -1,16 +1,34 @@
 package main
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-// Perimeter calculates the permiter of a rectangle given a height and width
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+// Perimeter calculates the perimiter of a rectangle given a height and width
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
 // Area calculates the area of a rectangle given the width and height
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * (c.Radius)
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
