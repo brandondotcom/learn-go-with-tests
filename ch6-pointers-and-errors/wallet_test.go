ee0b283cb592bacfe339b871ba0d04369abaeb54
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-26 08:57:53 -0400

    Refactor assert balance in tests

--- ch6-pointers-and-errors/wallet_test.go
+++ ch6-pointers-and-errors/wallet_test.go
@@ -3,31 +3,33 @@ package pointers_and_errors
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
-		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
+		err := wallet.Withdraw(Bitcoin(10))
+		if err != nil {
+			return
+		}
 
 		want := Bitcoin(10)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 }
