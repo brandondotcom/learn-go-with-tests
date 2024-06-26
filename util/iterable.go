01bb6b3194d8f1c7848e24af0662dac09c2574e3
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-26 08:27:55 -0400

    WIP - Filter and Map for iterables

--- util/iterable.go
+++ util/iterable.go
@@ -1,14 +1,48 @@
 package util
 
+// region Iterable
+
+// Accumulate is any function that returns a single value given two values of the same type.
 type Accumulate[T any] func(acc, next T) T
+
+// Predicate is a function that implements a conditional check on a single value and returns the boolean result.
+type Predicate[T any] func(t T) bool
+
+// Iterable is a collection whose elements can be iterated over.
 type Iterable[T any] interface {
-	// Fold Accumulates value starting w/ initial and applying operation from left to right
+
+	// Filter returns a new list containing all elements of this Iterable that match a given Predicate.
+	Filter(pred Predicate[T]) Iterable[T]
+
+	// Fold accumulates value starting w/ initial and applying operation from left to right
 	Fold(initial T, operation Accumulate[T]) T
 
-	// Reduce Accumulates value applying operation from left to right
+	// Reduce accumulates values applying operation from left to right
 	Reduce(operation Accumulate[T]) T
 }
 
+// endregion
+
+// region Pure Functions (Generic)
+
+// Transform is any function that determines a return value given any input of a given type
+type Transform[T, R any] func(in T) (out R)
+
+// Map returns a List whose elements are the result of applying a Transform function to each element of a given List.
+func Map[T, R any](l List[T], transform Transform[T, R]) List[R] {
+	out := make([]R, len(l))
+	for i, x := range l {
+		out[i] = transform(x)
+	}
+
+	return out
+}
+
+// endregion
+
+// region List
+
+// List implements the Iterable interface using slices.
 type List[T any] []T
 
 func (l List[T]) Fold(initial T, operation Accumulate[T]) T {
@@ -26,3 +60,5 @@ func (l List[T]) Reduce(operation Accumulate[T]) T {
 		return l[1:].Fold(l[0], operation)
 	}
 }
+
+// endregion
--- util/iterable_test.go
+++ util/iterable_test.go
@@ -1,6 +1,9 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExampleReduce() {
 	multiply := func(x, y int) int { return x * y }
@@ -17,4 +20,68 @@ func ExampleFold() {
 
 	product := multipliers.Fold(multiplicand, multiply)
 	fmt.Println(product) // Output: 168
+}
+
+func TestList_Fold(t *testing.T) {
+	t.Run("with a function reference", func(t *testing.T) {
+
+	})
+
+	t.Run("multiply integers", func(t *testing.T) {
+
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+
+	})
+
+	t.Run("with a closure capturing external scope", func(t *testing.T) {
+
+	})
+
+	t.Run("uncasted slice", func(t *testing.T) {
+
+	})
+
+	t.Run("empty list", func(t *testing.T) {
+
+	})
+}
+
+func TestList_Reduce(t *testing.T) {
+	t.Run("with an array", func(t *testing.T) {
+
+	})
+
+	t.Run("with pre-allocated slice", func(t *testing.T) {
+
+	})
+
+	t.Run("with a function reference", func(t *testing.T) {
+
+	})
+
+	t.Run("multiply integers", func(t *testing.T) {
+
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+
+	})
+
+	t.Run("uncasted slice", func(t *testing.T) {
+
+	})
+
+	t.Run("empty list - integer default value", func(t *testing.T) {
+
+	})
+
+	t.Run("empty list - string default value", func(t *testing.T) {
+
+	})
+
+	t.Run("empty list - struct default value", func(t *testing.T) {
+
+	})
 }
