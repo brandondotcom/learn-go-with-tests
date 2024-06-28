bbf89217d9110b07865f9925a5729f3bd88dcc61
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-28 09:06:28 -0400

    ch7 - return errors for word conflicts

--- ch7-maps/dictionary.go
+++ ch7-maps/dictionary.go
@@ -1,10 +1,21 @@
 package main
 
-import "errors"
+import (
+	"errors"
+)
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -15,6 +26,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch {
+	case errors.Is(err, ErrNotFound):
+		d[word] = definition
+	case err == nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
