9c75e5b99282ab4e0269318a3c34a300017e895d
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-06-28 08:59:40 -0400

    ch7 - refactor, dedicated error type

--- ch7-maps/dictionary_test.go
+++ ch7-maps/dictionary_test.go
@@ -13,15 +13,20 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
-
-		if err == nil {
+		_, got := dictionary.Search("unknown")
+		if got == nil {
 			t.Fatal("expected to get an error.")
 		}
-
-		assertStrings(t, err.Error(), want)
+		assertError(t, got, ErrNotFound)
 	})
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string) {
