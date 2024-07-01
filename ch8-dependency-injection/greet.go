3e7b2bd7a01bc33bc265aa90f4f5ee2ef26457c6
Brandon Nguyen <brandon.nguyen@dailypay.com>
2024-07-01 08:23:13 -0400

    [ch7]extra implement Greet using functional options pattern

--- ch8-dependency-injection/greet.go
+++ ch8-dependency-injection/greet.go
@@ -5,16 +5,67 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
-func Greet(writer io.Writer, name string) {
-	fmt.Fprintf(writer, "Hello, %s", name)
+// greeter holds the config/dependencies for the Greet function.
+type greeter struct {
+	writer io.Writer
+	name   string
 }
 
+func (g *greeter) greet() {
+	fmt.Fprintf(g.writer, "Hello, %s", g.name)
+}
+
+// greetOption is any function that takes a single argument reference to a greeter.
+// It can be used as a return value for functions that implement the Functional Options pattern.
+// see: https://golang.cafe/blog/golang-functional-options-pattern.html
+type greetOption func(*greeter)
+
+func (g *greeter) setDefaults() {
+	g.writer = os.Stdout
+	g.name = "World"
+}
+
+// region options
+
+func WithWriter(writer io.Writer) greetOption {
+	return func(g *greeter) {
+		g.writer = writer
+	}
+}
+
+func WithName(name string) greetOption {
+	return func(g *greeter) {
+		g.name = name
+	}
+}
+
+// endregion
+
+func Greet(options ...greetOption) {
+	g := &greeter{}
+	g.setDefaults()
+
+	for _, o := range options {
+		o(g)
+	}
+
+	g.greet()
+}
+
+// region main
+
 func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
-	Greet(w, "world")
+	Greet(
+		WithWriter(w),
+		WithName("world"),
+	)
 }
 
 func main() {
 	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
+
+// endregion
--- ch8-dependency-injection/greet_test.go
+++ ch8-dependency-injection/greet_test.go
@@ -2,17 +2,81 @@ package main
 
 import (
 	"bytes"
+	"io"
+	"os"
 	"testing"
 )
 
-func TestGreet(t *testing.T) {
-	buffer := bytes.Buffer{}
-	Greet(&buffer, "Chris")
+// yuck
+func captureStdout() (r *os.File, w *os.File, stdout *os.File) {
+	stdout = os.Stdout
+	r, w, _ = os.Pipe()
+	os.Stdout = w
 
-	got := buffer.String()
-	want := "Hello, Chris"
+	return
+}
+
+func readAndRestoreStdout(r, w, stdout *os.File) string {
+	w.Close()
+	out, _ := io.ReadAll(r)
+	os.Stdout = stdout
+
+	return string(out)
+}
+
+func assertCorrectOutput(t *testing.T, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func TestGreet(t *testing.T) {
+	t.Run("with defaults when no options specified", func(t *testing.T) {
+		r, w, stdout := captureStdout()
+
+		Greet()
+
+		got := readAndRestoreStdout(r, w, stdout)
+		want := "Hello, World"
+
+		assertCorrectOutput(t, got, want)
+	})
+
+	t.Run("with name", func(t *testing.T) {
+		r, w, stdout := captureStdout()
+
+		Greet(WithName("Brandon"))
+
+		got := readAndRestoreStdout(r, w, stdout)
+		want := "Hello, Brandon"
+
+		assertCorrectOutput(t, got, want)
+	})
+
+	t.Run("with writer", func(t *testing.T) {
+		buffer := bytes.Buffer{}
+
+		Greet(WithWriter(&buffer))
+
+		got := buffer.String()
+		want := "Hello, World"
+
+		assertCorrectOutput(t, got, want)
+	})
+
+	t.Run("with all options", func(t *testing.T) {
+		buffer := bytes.Buffer{}
+
+		Greet(
+			WithName("Chris"),
+			WithWriter(&buffer),
+		)
+
+		got := buffer.String()
+		want := "Hello, Chris"
+
+		assertCorrectOutput(t, got, want)
+	})
 }
